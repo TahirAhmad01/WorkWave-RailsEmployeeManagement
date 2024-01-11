fb62461a062d9f313352e3b8d8433085cbc6380d
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-01-11 12:11:21 +0600

    docoument form fixed

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,7 @@
 class Document < ApplicationRecord
+  DOC_TYPES = ["marksheet", "education", "photo id", "address proof", "others"].freeze
+
   belongs_to :employee
+  validates :name, :doc_type, :employee_id, presence: true
+  validates_presence_of :employee
 end
