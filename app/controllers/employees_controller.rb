08c11f1fdda9bb8e8b3b754907bc2322b460154a
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-01-16 12:57:42 +0600

    employe index view change

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -42,7 +42,7 @@ class EmployeesController < ApplicationController
 
   def employee_params
     params.require(:employee).permit(
-      :first_name,:middle_name,:last_name,:personal_email,:city,:state,:country,:pincode,:address_line_1,:address_line_2,
+      :first_name,:middle_name,:last_name,:personal_email,:city,:state,:country,:pincode,:address_line_1,:address_line_2, :date_of_birth, :date_of_joining, :job_title, :bio
     )
   end
 
