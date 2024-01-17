c6373015fac3b1fdc5b5428c5bd9debc5d95f2d4
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-01-17 12:36:45 +0600

    authentication page layout change

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: "users/registrations"
+  }
   root 'pages#home'
   get "about-us" => 'pages#about_us'
   get "contact-us" => 'pages#contact_us'
